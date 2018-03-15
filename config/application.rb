22b194b645035382fb3f4d1d9b20d873043096c8
Peter Boling <peter.boling@gmail.com>
2018-03-15 03:14:39 -0700

    Trying to get it running

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,20 @@
-require "bundler/setup"
-Bundler.require
-require "rails/all"
+require_relative 'boot'
 
-module PgHeroSolo
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FiveOne
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
     routes.append do
       mount PgHero::Engine, at: "/"
     end
--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,40 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
 threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked webserver processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+workers Integer(ENV['PUMA_WORKERS'] || 3)
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory. If you use this option
+# you need to make sure to reconnect any threads in the `on_worker_boot`
+# block.
+#
 preload_app!
 
 rackup DefaultRackup
+
 if ENV['BIND']
   bind ENV['BIND']
 else
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+  #
   port ENV['PORT'] || 3000
 end
+
+# Specifies the `environment` that Puma will run in.
+#
 environment ENV['RACK_ENV'] || 'production'
 
 unless ENV['DATABASE_URL']
@@ -19,6 +45,25 @@ unless ENV['DATABASE_URL']
   end
 end
 
+# If you are preloading your application and using Active Record, it's
+# recommended that you close any connections to the database before workers
+# are forked to prevent connection leakage.
+#
+# For PgHero we do not need to do this if ENV['DATABASE_URL'] == 'nulldb:///'
+#
+unless ENV['DATABASE_URL'] == 'nulldb:///'
+  before_fork do
+    ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+  end
+end
+
+# The code in the `on_worker_boot` will be called if you are using
+# clustered mode by specifying a number of `workers`. After each worker
+# process is booted, this block will be run. If you are using the `preload_app!`
+# option, you will want to use this block to reconnect to any threads
+# or connections that may have been created at application boot, as Ruby
+# cannot share connections between processes.
+#
 on_worker_boot do
   # worker specific setup
   ActiveSupport.on_load(:active_record) do
