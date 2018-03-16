614a6570eaab45f49d57e4fab969c9b5aabd131a
Peter Boling <peter.boling@gmail.com>
2018-03-15 23:56:16 -0700

    documentation

--- config/deploy.rb
+++ config/deploy.rb
@@ -119,10 +119,10 @@ end
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
-# Uncomment the following to require manually verifying the host key before first deploy.
 set :ssh_options, {
     keys: %w(~/pems/code_deploy.pem),
     forward_agent: true,
+    # Require manually verifying the host key before first deploy?
     verify_host_key: :secure
 }
 
