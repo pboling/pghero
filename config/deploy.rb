224687a293462c9eb7ea758b685b9b97cb5519d8
Peter Boling <peter.boling@gmail.com>
2018-03-16 00:20:02 -0700

    remove redundant comment

--- config/deploy.rb
+++ config/deploy.rb
@@ -91,9 +91,6 @@ end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
